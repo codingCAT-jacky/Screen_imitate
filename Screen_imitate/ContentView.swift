ff6eb1415a55594d8da0568272cf5f5ea2dcbd63
JackyLai <jacky8883938@gmail.com>
2022-10-03 20:54:08 +0800

    Initial Commit

---
+++ Screen_imitate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Screen_imitate
+//
+//  Created by User16 on 2022/10/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Screen_imitate/Screen_imitateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_imitateApp.swift
+//  Screen_imitate
+//
+//  Created by User16 on 2022/10/3.
+//
+
+import SwiftUI
+
+@main
+struct Screen_imitateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
